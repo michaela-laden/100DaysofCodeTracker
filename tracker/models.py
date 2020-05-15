ebc95cd5bbb73adb2be20c90f1b88cf6736d7193
Michaela Laden <michaela.laden@gmail.com>
2020-05-14 21:00:40 -0400

    fix admin page and set up api

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Log(models.Model):
+    date_posted = models.DateTimeField(default=timezone.now)
+    worked_on = models.TextField()
+    hours_spent = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
